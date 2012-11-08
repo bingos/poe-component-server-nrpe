5d9fb4b093daea967ba3d4b9da99bb967987b749
Rocco Caputo <rcaputo@cpan.org>
2012-11-08 14:26:08 -0500

    Fix the SYNOPSIS and example to exit() rather than return() from plugins.

--- lib/POE/Component/Server/NRPE.pm
+++ lib/POE/Component/Server/NRPE.pm
@@ -564,6 +564,7 @@ POE::Component::Server::NRPE - A POE Component implementation of NRPE Daemon.
   use strict;
   use POE;
   use POE::Component::Server::NRPE;
+  use POE::Component::Server::NRPE::Constants qw(NRPE_STATE_OK);
 
   my $port = 5666;
   
@@ -571,14 +572,14 @@ POE::Component::Server::NRPE - A POE Component implementation of NRPE Daemon.
 	port => $port;
   );
 
-  $nrped->add_command( command => meep, program => \&_meep );
+  $nrped->add_command( command => 'meep', program => \&_meep );
 
   $poe_kernel->run();
   exit 0;
 
   sub _meep {
-    print STDOUT "OK meep\n";
-    return 0;
+	print STDOUT "OK meep\n";
+	exit NRPE_STATE_OK;
   }
 
 =head1 DESCRIPTION
